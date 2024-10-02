36c1268f799929953dc3288b69743bbaf3b2b592
Nizar-official-Github <nizar.ajroud@gmail.com>
2024-10-02 13:50:54 -0400

    Update gh-push-repo.bash

--- gh-push-repo.bash
+++ gh-push-repo.bash
@@ -10,12 +10,14 @@ mkdir $REPO_NAME
 
 cd $REPO_NAME
 git init
+echo "# My New Project $REPO_NAME" > README.md
 git add .
 git commit -m "initial commit"
 
 #create remote repository and origin
 
 gh repo create $REPO_NAME --private
+sleep 10
 git remote add origin https://github.com/nizarajroud/$REPO_NAME.git
 git push --set-upstream origin main
 git push
